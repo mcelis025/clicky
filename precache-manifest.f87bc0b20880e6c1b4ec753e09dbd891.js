480c9a5627fc23ed3676b16944a9548443906143
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-08-01 12:44:02 -0700

    Updates

---
+++ precache-manifest.f87bc0b20880e6c1b4ec753e09dbd891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2bc78183736786486aeeda83dd8834",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "51af88647c4b55d8009a",
+    "url": "/clicky/static/css/main.178be59a.chunk.css"
+  },
+  {
+    "revision": "5852b2fc388de59d8c9a",
+    "url": "/clicky/static/js/2.97acdedf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky/static/js/2.97acdedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51af88647c4b55d8009a",
+    "url": "/clicky/static/js/main.3ad8d2ba.chunk.js"
+  },
+  {
+    "revision": "9868e024bc5f9d90297f",
+    "url": "/clicky/static/js/runtime-main.8db5a474.js"
+  }
+]);
